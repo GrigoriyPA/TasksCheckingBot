284bcc4597292f2301403b68f78315eb851ce67b
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-01-14 13:59:27 +0300

    Add default main

---
+++ main.py
@@ -0,0 +1,13 @@
+import locale
+#from TelegramClient import TelegramClient
+
+
+def main():
+    locale.setlocale(locale.LC_ALL, locale.normalize("RU"))
+
+    #bot = TelegramClient()
+    #bot.run()
+
+
+if __name__ == "__main__":
+    main()
