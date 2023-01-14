7198f406941db96d0e07e37456fd60271ee1418b
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-01-14 16:26:20 +0300

    Added authorization

--- telegram_logic/telegram_client.py
+++ telegram_logic/telegram_client.py
@@ -1,15 +1,93 @@
 import threading
 import telebot
+from telebot import types
 import config
+import constants
+
+
+class WaitModeDescription:
+    def __init__(self, function, status=None, data=None):
+        self.function = function
+        self.status = status
+        self.data = data
 
 
 class TelegramClient:
     def __init__(self):
         self.client = None
         self.handler_thread = None
+        self.wait_mode = dict()
+
+    def __is_super_admin(self, id):
+        return False
+
+    def __is_admin(self, id):
+        return False
+
+    def __is_valid_login(self, login):
+        return False
+
+    def __is_valid_password(self, login, password):
+        return False
+
+    def __is_authorized(self, id):
+        return False
+
+    def __sign_out(self, login):
+        return 0  # delogined id (None if not)
+
+    def __sign_up(self, login, id):
+        pass
+
+    def __get_markup(self, message):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        if not self.__is_authorized(message.chat.id):
+            markup.add(types.KeyboardButton(text="Авторизоваться"))
+
+        return markup
 
     def __compute_command_start(self, message):
-        self.client.send_message(message.chat.id, text="Hello!")
+        if not self.__is_authorized(message.chat.id):
+            text = "Пожалуйста, авторизуйтесь."
+
+        self.client.send_message(message.chat.id, text=text, reply_markup=self.__get_markup(message))
+
+    def __compute_keyboard_sign_up(self, message):
+        if self.__is_authorized(message.chat.id):
+            self.client.send_message(message.chat.id, text="Вы уже были авторизованны.")
+            return
+
+        if self.wait_mode[message.chat.id] is None:
+            self.wait_mode[message.chat.id] = WaitModeDescription(self.__compute_keyboard_sign_up, status="WAIT_LOGIN")
+            self.client.send_message(message.chat.id, text="Введите лигон аккаунта для авторизации:")
+            return
+
+        if self.wait_mode[message.chat.id].status == "WAIT_LOGIN":
+            if not self.__is_valid_login(message.text):
+                self.wait_mode[message.chat.id] = None
+                self.client.send_message(message.chat.id, text="Введённый логин не существует, авторизация отменена.")
+                return
+
+            self.wait_mode[message.chat.id] = WaitModeDescription(self.__compute_keyboard_sign_up, status="WAIT_PASSWORD", data=message.text)
+            self.client.send_message(message.chat.id, text="Введите пароль:")
+            return
+
+        if self.wait_mode[message.chat.id].status == "WAIT_PASSWORD":
+            login = self.wait_mode[message.chat.id].data
+            password = message.text
+
+            self.wait_mode[message.chat.id] = None
+            if not self.__is_valid_password(login, password):
+                self.client.send_message(message.chat.id, text="Введённый неправильный пароль, авторизация отменена.")
+                return
+
+            last_id = self.__sign_out(login)
+            if last_id is not None:
+                self.client.send_message(last_id, text="В ваш профиль выполнен вход с другого телеграм аккаунта, вы были разлогинены.")
+
+            self.__sign_up(login, message.chat.id)
+            self.client.send_message(message.chat.id, text="Успешная авторизация!", reply_markup=self.__get_markup(message))
+            return
 
     def __handler(self):
         print("TG client started.")
@@ -23,6 +101,11 @@ class TelegramClient:
             # If message from chet - skip
             if message.chat.id != message.from_user.id:
                 return
+
+            if self.wait_mode[message.chat.id] is not None:
+                self.wait_mode[message.chat.id]["function"](message)
+            elif message.text == "Авторизоваться":
+                self.__compute_keyboard_sign_up(message)
 
         self.client.infinity_polling()
 
