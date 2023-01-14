856e9f343e53c93daa4699627626f38c68fd2a66
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-01-14 18:50:57 +0300

    Added login command

--- telegram_logic/telegram_client.py
+++ telegram_logic/telegram_client.py
@@ -7,7 +7,7 @@ import constants
 
 
 def add_error_to_log(text):
-    error_log = open(constants.ERROR_LOG_NAME, "a")
+    error_log = open(constants.PATH_TO_ERROR_LOG + constants.ERROR_LOG_NAME, "a")
     error_log.write(text + "\n\n")
     error_log.close()
 
@@ -25,8 +25,14 @@ class TelegramClient:
         self.handler_thread = None
         self.wait_mode = dict()
 
-        self.data_base = DatabaseHelper(constants.DATABASE_NAME)
+        self.data_base = DatabaseHelper(constants.PATH_TO_DATABASE, constants.DATABASE_NAME)
         # self.data_base.create_database()
+
+    def __is_super_admin(self, id):
+        return self.data_base.get_user_by_telegram_id(id).status == constants.SUPER_ADMIN
+
+    def __is_admin(self, id):
+        return self.__is_super_admin(id) or self.data_base.get_user_by_telegram_id(id).status == constants.ADMIN
 
     def __is_valid_login(self, login):
         return self.data_base.get_user_by_login(login) is not None
@@ -44,27 +50,44 @@ class TelegramClient:
             return None
         return user.login
 
-    def __sign_out(self, login):
-        user = self.data_base.get_user_by_login(login)
+    def __get_id_by_login(self, id):
+        user = self.data_base.get_user_by_login(id)
 
-        if user is not None:
-            return user.telegram_id
-        return None
+        if user is None:
+            return None
+        return user.telegram_id
 
-    def __sign_up(self, login, id):
-        pass
-
-    def __get_markup(self, id):
+    def __get_markup(self, message):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        if self.wait_mode[id] is not None:
-            return markup
+        if self.wait_mode[message.chat.id] is not None:
+            return types.ReplyKeyboardRemove()
 
-        if not self.__is_authorized(id):
+        if not self.__is_authorized(message.chat.id):
             markup.add(types.KeyboardButton(text="Авторизоваться"))
         else:
             markup.add(types.KeyboardButton(text="Выйти"))
 
         return markup
+
+    def __add_user(self, id):
+        if id not in self.wait_mode:
+            self.wait_mode[id] = None
+
+    def __sign_out(self, id):
+        user = self.data_base.get_user_by_telegram_id(id)
+        if user is None:
+            add_error_to_log("Invalid id in function __sign_out")
+            return None
+
+        self.data_base.change_user_telegram_id(user.login, constants.UNAUTHORIZED_TELEGREM_ID)
+
+    def __sign_up(self, login, id):
+        user = self.data_base.get_user_by_login(login)
+        if user is None:
+            add_error_to_log("Invalid login in function __sign_out")
+            return None
+
+        self.data_base.change_user_telegram_id(user.login, id)
 
     def __send_message(self, id, text, markup=None):
         if markup is None:
@@ -85,20 +108,19 @@ class TelegramClient:
 
         if self.wait_mode[message.chat.id] is None:
             self.wait_mode[message.chat.id] = WaitModeDescription(self.__compute_keyboard_sign_up, status="WAIT_LOGIN")
-            self.client.send_message(message.chat.id, text="Введите логин аккаунта для авторизации:",
-                                     markup=self.__get_markup(message))
+            self.__send_message(message.chat.id, "Введите логин аккаунта для авторизации:",
+                                markup=self.__get_markup(message))
             return
 
         if self.wait_mode[message.chat.id].status == "WAIT_LOGIN":
             if not self.__is_valid_login(message.text):
                 self.wait_mode[message.chat.id] = None
-                self.client.send_message(message.chat.id, text="Введённый логин не существует, авторизация отменена.",
-                                         markup=self.__get_markup(message))
+                self.__send_message(message.chat.id, "Введённый логин не существует, авторизация отменена.", markup=self.__get_markup(message))
                 return
 
             self.wait_mode[message.chat.id] = WaitModeDescription(self.__compute_keyboard_sign_up,
                                                                   status="WAIT_PASSWORD", data=message.text)
-            self.client.send_message(message.chat.id, text="Введите пароль:", markup=self.__get_markup(message))
+            self.__send_message(message.chat.id, "Введите пароль:", markup=self.__get_markup(message))
             return
 
         if self.wait_mode[message.chat.id].status == "WAIT_PASSWORD":
@@ -107,30 +129,27 @@ class TelegramClient:
 
             self.wait_mode[message.chat.id] = None
             if not self.__is_valid_password(login, password):
-                self.client.send_message(message.chat.id, text="Введённый неправильный пароль, авторизация отменена.",
-                                         markup=self.__get_markup(message))
+                self.__send_message(message.chat.id, "Введённый неправильный пароль, авторизация отменена.", markup=self.__get_markup(message))
                 return
 
-            last_id = self.__sign_out(login)
-            if last_id is not None:
-                self.client.send_message(last_id,
-                                         text="В ваш профиль выполнен вход с другого телеграм аккаунта, вы были разлогинены.")
+            id = self.__get_id_by_login(login)
+            if id is not None and id != constants.UNAUTHORIZED_TELEGREM_ID:
+                self.__send_message(id, "В ваш профиль выполнен вход с другого телеграм аккаунта, вы были разлогинены.")
 
             self.__sign_up(login, message.chat.id)
-            self.client.send_message(message.chat.id, text="Успешная авторизация!",
-                                     reply_markup=self.__get_markup(message))
+            self.__send_message(message.chat.id, "Успешная авторизация!", markup=self.__get_markup(message))
             return
 
         self.wait_mode[message.chat.id] = None
-        add_error_to_log("Invalid wait status \'" + str(self.wait_mode[message.chat.id].status) + "\' in function __compute_keyboard_sign_up")
+        add_error_to_log("Invalid wait status \'" + str(
+            self.wait_mode[message.chat.id].status) + "\' in function __compute_keyboard_sign_up")
 
     def __compute_keyboard_sign_out(self, message):
-        login = self.__get_login_by_id(message.chat.id)
-        if login is None:
+        if not self.__is_authorized(message.chat.id):
             self.__send_message(message.chat.id, "Вы не авторизованны.", markup=self.__get_markup(message))
             return
 
-        self.__sign_out(login)
+        self.__sign_out(message.chat.id)
         self.__send_message(message.chat.id, "Вы вышли из аккаунта.", markup=self.__get_markup(message))
 
     def __handler(self):
@@ -138,20 +157,24 @@ class TelegramClient:
 
         @self.client.message_handler(commands=['start'])
         def start(message):
+            self.__add_user(message.chat.id)
             self.__compute_command_start(message)
 
         @self.client.message_handler(content_types=["text"])
         def on_message(message):
-            # If message from chet - skip
+            # If message from chat -> skip
             if message.chat.id != message.from_user.id:
                 return
+            self.__add_user(message.chat.id)
 
             if self.wait_mode[message.chat.id] is not None:
-                self.wait_mode[message.chat.id]["function"](message)
+                self.wait_mode[message.chat.id].function(message)
             elif message.text == "Авторизоваться":
                 self.__compute_keyboard_sign_up(message)
             elif message.text == "Выйти":
                 self.__compute_keyboard_sign_out(message)
+            else:
+                self.__send_message(message.chat.id, "Неизвестная команда.")
 
         self.client.infinity_polling()
 
