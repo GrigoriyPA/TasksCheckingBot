3be0c96276de958d4ec89c91945d9b616632a7bf
Artem Safin <artemsafin67@yandex.ru>
2023-01-16 03:32:38 +0300

    added more funcs with database, str operators for user and homework classes, done TODOs, added throwing exceptions in some places

--- homework.py
+++ homework.py
@@ -2,3 +2,6 @@ class Homework:
     def __init__(self, name: str, right_answers: list[str]):
         self.name: str = name
         self.right_answers: list[str] = right_answers
+
+    def __str__(self):
+        return f"Homework: name = {self.name}, right_answers = {'; '.join(self.right_answers)}"
--- user.py
+++ user.py
@@ -4,3 +4,7 @@ class User:
         self.password: str = password
         self.status: str = status
         self.telegram_id: int = telegram_id
+
+    def __str__(self):
+        return f"User: login = {self.login}, password = {self.password}, " \
+               f"status = {self.status}, telegram_id = {self.telegram_id}"
