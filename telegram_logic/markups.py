1e41e250e52e1ef42aa018bc043172af2d3a40f1
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-01-16 13:00:57 +0300

    Updated results_table

--- telegram_logic/markups.py
+++ telegram_logic/markups.py
@@ -21,24 +21,48 @@ def get_all_homeworks(homework_list, callback_data):
 
 def get_results_table(results, homework_name, homework_size):
     keyboard = []
-    row = [types.InlineKeyboardButton(text=" ", callback_data="NONE")]
+    row = [types.InlineKeyboardButton(text=" ", callback_data="NONE"), types.InlineKeyboardButton(text="Σ", callback_data="NONE")]
     for i in range(1, homework_size + 1):
         row.append(types.InlineKeyboardButton(text=str(i), callback_data="NONE"))
     keyboard.append(row)
 
+    amount = 0
+    row_id = 0
+    rows = []
+    rows_order = []
+    sum_in_column = [0] * homework_size
     for result in results:
         row = [types.InlineKeyboardButton(text=result[0].login, callback_data="NONE")]
         task_id = 0
+        current_sum = 0
         for answer in result[1]:
             task_id += 1
             if answer[0] is None or answer[0] == '':
                 row.append(types.InlineKeyboardButton(text=" ", callback_data="NONE"))
             elif answer[0] == answer[1]:
+                current_sum += 1
+                sum_in_column[task_id - 1] += 1
                 row.append(types.InlineKeyboardButton(text="✅", callback_data="SHOW_TASK_FOR_ADMIN$" + result[0].login + "$" + homework_name + "$" + str(task_id)))
             else:
                 row.append(types.InlineKeyboardButton(text="❌", callback_data="SHOW_TASK_FOR_ADMIN$" + result[0].login + "$" + homework_name + "$" + str(task_id)))
 
-        keyboard.append(row)
+        row = row[:1] + [types.InlineKeyboardButton(text=str(current_sum), callback_data="NONE")] + row[1:]
+        rows_order.append((-current_sum, result[0].login, row_id))
+        amount += current_sum
+        row_id += 1
+
+        rows.append(row)
+    rows_order.sort()
+
+    for element in rows_order:
+        keyboard.append(rows[element[2]])
+
+    row = [types.InlineKeyboardButton(text="Σ", callback_data="NONE"), types.InlineKeyboardButton(text=str(amount), callback_data="NONE")]
+    for i in range(homework_size):
+        row.append(types.InlineKeyboardButton(text=str(sum_in_column[i]), callback_data="NONE"))
+    keyboard.append(row)
+
+    #keyboard.append([types.InlineKeyboardButton(text="Σ", callback_data="NONE")])
 
     return types.InlineKeyboardMarkup(keyboard)
 
