627c271a539b7e25d5581eda782087f537995102
Grigoriy Pisarenko <ga_pisarenko@mail.ru>
2023-01-21 19:57:15 +0300

    Added TelegramClient class

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,11 @@
 import locale
-from bot.telegram_logic.telegram_client import TelegramClient
+from telegram_logic.user_handler import UserHandler
 
 
 def main():
     locale.setlocale(locale.LC_ALL, locale.normalize("RU"))
 
-    bot = TelegramClient()
+    bot = UserHandler()
     bot.run()
 
 
---
+++ bot/telegram_logic/inner_types.py
@@ -0,0 +1,42 @@
+import requests
+from telebot import types
+from typing import Optional, Union
+
+MARKUP_TYPES = Optional[
+    Union[types.InlineKeyboardMarkup, types.ReplyKeyboardMarkup, types.ReplyKeyboardRemove, types.ForceReply]]
+
+
+class Attachment:
+    def __init__(self, attach_type: str, link: str) -> None:
+        self.attach_type = attach_type
+        self.link = link
+
+    def get_content(self) -> bytes:
+        # Returns data of current link
+
+        return requests.get(self.link).content
+
+
+class Message:
+    def __init__(self, from_id: int, text: str, author: types.ChatMember, attachment: Optional[Attachment] = None,
+                 chat_name: Optional[str] = None) -> None:
+        self.from_id = from_id
+        self.text = text
+        self.author = author
+        self.attachment = attachment
+        self.chat_name = chat_name
+
+    def is_from_chat(self) -> bool:
+        return self.from_id == self.author.user.id
+
+
+class Callback:
+    def __init__(self, from_id: int, callback_data: str, message_id: int, text: str, author: types.ChatMember) -> None:
+        self.from_id = from_id
+        self.callback_data = callback_data
+        self.message_id = message_id
+        self.text = text
+        self.author = author
+
+    def is_from_chat(self) -> bool:
+        return self.from_id == self.author.user.id
